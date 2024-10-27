5da18f486512e86b545555e6a2867b3f085e0063
joshikrishita <joshikrishita@outlook.com>
2024-10-27 15:42:28 +0530

    authReact

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <input type="text" id="email"placeholder='Enter Email'></input><br></br>
+      <input type="password" id="password"placeholder='Enter password '></input><br></br>
+      <input type="text" id="firstName"placeholder='Enter firstName'></input><br></br>
+      <input type="text" id="lastName"placeholder='Enter lastName'></input><br></br>
+    </>
+  )
+}
+
+export default App
